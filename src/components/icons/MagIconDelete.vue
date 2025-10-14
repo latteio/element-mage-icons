16944155d4ee564342878cbd8e647026d2d16e3a
Latte <45244190@qq.com>
2025-10-14 18:41:08 +0800

    feat: 编制新的图标按钮;

---
+++ src/components/icons/MagIconDelete.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M358.4 102.4V0h307.2v102.4h307.2v102.4H51.2V102.4h307.2zM204.8 307.2v614.4h614.4V307.2h102.4v665.6a51.2 51.2 0 0 1-51.2 51.2H153.6a51.2 51.2 0 0 1-51.2-51.2V307.2h102.4z m153.6 102.4h102.4v409.6H358.4V409.6z m204.8 0h102.4v409.6h-102.4V409.6z"></path>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'MagIconDelete'
+}
+</script>
---
+++ src/components/icons/MagIconPlus.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+    <path d="M966.4 454.4h-396.8V57.6a57.6 57.6 0 0 0-115.2 0v396.8H57.6a57.6 57.6 0 0 0 0 115.2h396.8v396.8a57.6 57.6 0 0 0 115.2 0v-396.8h396.8a57.6 57.6 0 0 0 0-115.2z"></path>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'MagIconPlus'
+}
+</script>
